6f0576810a3007ed908cf0536ef4d18a1895b80f
ansq <3194100053@qq.com>
2021-02-05 15:54:55 +0800

    Only capture the specified program window

--- streammanager.h
+++ streammanager.h
@@ -1,117 +1,120 @@
 #ifndef STREAMMANAGER_H
 #define STREAMMANAGER_H
+
 #include <QIODevice>
-#include <QVariant>
-#include <QTcpSocket>
-#include <QJsonDocument>
-#include <QJsonObject>
-#include <QImage>
+#include <QDataStream>
 #include <QBuffer>
+#include <QAbstractSocket>
+#include <QJsonObject>
+#include <QJsonDocument>
+#include <QImage>
 
-inline void receive_map(QTcpSocket * socket, QVariantMap & map){
-    int expected_length = sizeof(int);
+class BlockReader
+{
+public:
+    BlockReader(QIODevice *io)
+    {
+        buffer.open(QIODevice::ReadWrite);
+        _stream.setVersion(QDataStream::Qt_4_8);
+        _stream.setDevice(&buffer);
 
-    char * buffer = new char[expected_length];
-    char * p = buffer;
-    while (expected_length > 0){
-        int len = socket->read(p, expected_length);
-        if (len <= 0){
-            socket->waitForReadyRead(-1);
-        }
-        else {
-            expected_length -= len;
-            p += len;
+        quint64 blockSize;
+
+        // Read the size.
+        readMax(io, sizeof(blockSize));
+        buffer.seek(0);
+        _stream >> blockSize;
+
+        // Read the rest of the data.
+        readMax(io, blockSize);
+        buffer.seek(sizeof(blockSize));
+    }
+
+    QDataStream& stream()
+    {
+        return _stream;
+    }
+
+private:
+    // Blocking reads data from socket until buffer size becomes exactly n. No
+    // additional data is read from the socket.
+    void readMax(QIODevice *io, int n)
+    {
+        while (buffer.size() < n) {
+            if (!io->bytesAvailable()) {
+                io->waitForReadyRead(30000);
+            }
+            buffer.write(io->read(n - buffer.size()));
         }
     }
-    expected_length = *((int *)(buffer));
-    delete []buffer;
+    QBuffer buffer;
+    QDataStream _stream;
+};
 
-    buffer = new char[expected_length];
-    p = buffer;
-    while (expected_length > 0){
-        int len = socket->read(p, expected_length);
-        if (len <= 0){
-            socket->waitForReadyRead(-1);
-        }
-        else {
-            expected_length -= len;
-            p += len;
-        }
+class BlockWriter
+{
+public:
+    BlockWriter(QIODevice *io)
+    {
+        buffer.open(QIODevice::WriteOnly);
+        this->io = io;
+        _stream.setVersion(QDataStream::Qt_4_8);
+        _stream.setDevice(&buffer);
+
+        // Placeholder for the size. We will get the value
+        // at the end.
+        _stream << quint64(0);
     }
-    QByteArray arr = QByteArray(buffer, (p - buffer));
 
-    delete []buffer;
+    ~BlockWriter()
+    {
+        // Write the real size.
+        _stream.device()->seek(0);
+        _stream << (quint64) buffer.size();
+
+        // Flush to the device.
+        io->write(buffer.buffer());
+    }
+
+    QDataStream &stream()
+    {
+        return _stream;
+    }
+
+private:
+    QBuffer buffer;
+    QDataStream _stream;
+    QIODevice *io;
+};
+
+inline void receive_map(QIODevice * socket, QVariantMap & map){
+    QByteArray arr;
+    BlockReader(socket).stream() >> arr;
     QJsonDocument doc = QJsonDocument::fromJson(arr);
     QJsonObject obj = doc.object();
     map = obj.toVariantMap();
 }
 
-inline void receive_image(QTcpSocket * socket, QImage & image){
-    int expected_length = sizeof(int);
-
-    char * buffer = new char[expected_length];
-    char * p = buffer;
-    while (expected_length > 0){
-        int len = socket->read(p, expected_length);
-        if (len <= 0){
-            socket->waitForReadyRead(-1);
-        }
-        else {
-            expected_length -= len;
-            p += len;
-        }
-    }
-    expected_length = *((int *)(buffer));
-    delete []buffer;
-
-    buffer = new char[expected_length];
-    p = buffer;
-    while (expected_length > 0){
-        int len = socket->read(p, expected_length);
-        if (len <= 0){
-            socket->waitForReadyRead(-1);
-        }
-        else {
-            expected_length -= len;
-            p += len;
-        }
-    }
-    QByteArray arr = QByteArray(buffer, (p - buffer));
-
-    delete []buffer;
-    image = QImage::fromData(arr);
-}
-
-inline void send_map(QTcpSocket * socket, QVariantMap & map){
+inline void send_map(QIODevice * socket, QVariantMap & map){
     QJsonObject obj = QJsonObject::fromVariantMap(map);
     QJsonDocument doc = QJsonDocument(obj);
     QByteArray arr = doc.toJson();
-    QByteArray len;
-    int length = arr.size();
-    for (unsigned i = 0; i < sizeof(int); ++i){
-        len += *(reinterpret_cast<char *>(&length) + i);
-    }
-
-    socket->write(len);
-    socket->write(arr);
-    socket->flush();
+    BlockWriter(socket).stream() << arr;
 }
 
-inline void send_image(QTcpSocket * socket, QImage & image){
+inline void receive_image(QIODevice * socket, QImage & image){
+    QByteArray arr;
+    BlockReader(socket).stream() >> arr;
+    image = QImage::fromData(arr);
+}
+
+inline void send_image(QIODevice * socket, QImage & image){
     QByteArray arr;
     QBuffer buffer(&arr);
     buffer.open(QIODevice::WriteOnly);
     image.save(&buffer, "png");
     buffer.close();
-    QByteArray len;
-    int length = arr.size();
-    for (unsigned i = 0; i < sizeof(int); ++i){
-        len += *(reinterpret_cast<char *>(&length) + i);
-    }
-    length = socket->write(len);
-    socket->waitForBytesWritten();
-    length = socket->write(arr);
-    socket->flush();
+    BlockWriter(socket).stream() << arr;
 }
 
-#endif // STREAMMANAGER_H
+#endif
